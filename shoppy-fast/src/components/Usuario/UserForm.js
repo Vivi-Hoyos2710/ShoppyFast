ebcf7101b89162bb6e8e1b158ae85dd9c4a692d4
csofia1408 <csrendonb@eafit.edu.co>
2022-10-21 23:31:50 -0500

    correción pequeña formulario

--- shoppy-fast/src/components/Usuario/UserForm.js
+++ shoppy-fast/src/components/Usuario/UserForm.js
@@ -9,7 +9,8 @@ const UserForm = ({ setInfoUser, infoUsuario }) => {
     const userSchema = yup.object().shape({
         name: yup.string().required("Campo de nombre vacio"),
         surname: yup.string().required("Campo de apellido vacio"),
-        document:yup.number("Debe ser un valor numerico").typeError("Campo de cédula vacio").positive("No puede ser un número negativo").integer("Debe ser entero"),
+        document:yup.number("Debe ser 
+        un valor numerico").typeError("Campo de cédula vacio").positive("No puede ser un número negativo").integer("Debe ser entero"),
         number: yup.number("Debe ser un valor numerico").typeError("Campo de cantidad del producto vacio").positive("No puede ser un número negativo").integer("Debe ser entero"),
         email: yup.string().email("No es un email valido").required("Campo de email vacio"),
         check:yup.bool(),
